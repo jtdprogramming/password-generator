f7599f5e2c98a49f30b537284681860223c8e97f
James D <jtd.programming@gmail.com>
2021-11-20 21:05:55 -0500

    file structure updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,37 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
+
+
+/*
+Quokka practice with functions
+*/
+
+
+// const person = {
+//   name: 'jim',
+//   age: 30,
+//   location: 'NY',
+// }
+
+// console.log(person);
+
+// function add(x, y){
+//   return x + y;
+// }
+
+// console.log(add(25, 54));
+
+// const nums = [11, 30, 22, 9, 14];
+
+// console.log(nums.pop());
+
+// const sum = nums.reduce((acc, cur) => acc + cur)
+
+// sum
