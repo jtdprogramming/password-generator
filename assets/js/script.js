d351a9ebcfe607909906e9ae609c92386a943a83
James D <jtd.programming@gmail.com>
2021-11-21 00:53:53 -0500

    criteria conditionals and force choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,28 +9,45 @@ function generatePassword() {
   var passLength = window.prompt('Please enter a numerical password length');
     
   // Get password length and confirm choices
-  if (passLength >= 8 && <= 128) {
+  if (passLength >= 8 && passLength <= 128) {
     var lowerCase = confirm("Include lower case letters?");
     var upperCase = confirm("Include upper case letters?");
     var numbers = confirm("Include numbers?");
     var special = confirm("Include special characters?");
   }
-
-  // Force criteria choice
-  if (lowerCase === false && upperCase === false && numbers === false && special === false){
-    window.alert("You must choose at least one")
+  else {
+    window.alert("Please choose a number between 8 and 128");
+    return generatePassword();
   }
 
+  // Force criteria choice ** update to return to "choice function" instead of asking for length
+  if (lowerCase === false && upperCase === false && numbers === false && special === false){
+    window.alert("You must choose at least one, try again!");
+    return generatePassword();
+  }
+
+  //conditionals to determine criteria
+  if (lowerCase) {
+    lowerCase = ["abcdefghijklmnopqrstuvwxyz"]
+  } else if (lowerCase === false) {
+    lowerCase = [""]
+  }
+
+  if (upperCase) {
+    upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+  } else if (upperCase === false) {
+    upperCase = [""]
+  }
+
+  if (numbers) {
+    numbers = ["0123456789"]
+  } else if (numbers === false) {
+    numbers = [""]
+  }
   
-  // Validate input
   // Generate random pass
   return password;
 }
-
-
-
-  
-
 
 
 // Write password to the #password input
@@ -39,41 +56,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password; //Display password value on screen
-
-  // generatePassword() function
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
-
-
-
-
-
-
-
-// const person = {
-//   name: 'jim',
-//   age: 30,
-//   location: 'NY',
-// }
-
-// console.log(person);
-
-// function add(x, y){
-//   return x + y;
-// }
-
-// console.log(add(25, 54));
-
-// const nums = [11, 30, 22, 9, 14];
-
-// console.log(nums.pop());
-
-// const sum = nums.reduce((acc, cur) => acc + cur)
-
-// sum
